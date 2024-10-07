784b2844bce86d89f49ee049c777175bb1bfc43e
SanPut <santeriandre@gmail.com>
2024-10-07 18:17:46 +0300

    task7.sql: answer

--- Exercise 1/task7.sql
+++ Exercise 1/task7.sql
@@ -1,2 +1,12 @@
 -- Fetch all the tracks which length is less than one minute or more than five minutes.
 -- Sort the result in ascending order by the length.
+select
+  name
+  , length
+from
+  track
+where
+  length < 100
+  or length > 500
+order by
+  length asc;
