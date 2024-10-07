01cf78dff3ab1d28dd1750f87b5d898f32b304c6
SanPut <santeriandre@gmail.com>
2024-10-07 18:18:01 +0300

    task9.sql: answer

--- Exercise 1/task9.sql
+++ Exercise 1/task9.sql
@@ -4,3 +4,11 @@
 -- - headline
 -- - review text
 -- - a reference to the album id which is reviewed, not null
+create table review (
+  id int primary key auto increment
+  , reviewer_name varchar(255) not null
+  , headline varchar(255)
+  , review_text TEXT
+  , album_id int not null
+  , foreign key (album_id) references album(id)
+);
