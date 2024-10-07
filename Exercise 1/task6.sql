e0a9d7dd81fd0820e33ec0f12453fdf4a25d2e6c
SanPut <santeriandre@gmail.com>
2024-10-07 18:17:35 +0300

    task6.sql: answer

--- Exercise 1/task6.sql
+++ Exercise 1/task6.sql
@@ -1,3 +1,14 @@
 -- Fetch all the tracks which name starts with the word "The" and are at least one minute long.
--- Sort the result primarily in ascending order by the name and secondarily 
+-- Sort the result primarily in ascending order by the name and secondarily
 -- in ascending order by the length.
+select
+  name
+  , length
+from
+  track
+where
+  name like "the%"
+  and length >= 100
+order by
+  name asc
+  , length asc;
