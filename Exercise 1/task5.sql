268e64a7ee60711e3e4f6719f83a0d0c7e8a01f4
SanPut <santeriandre@gmail.com>
2024-10-07 18:17:24 +0300

    task5.sql: answer

--- Exercise 1/task5.sql
+++ Exercise 1/task5.sql
@@ -1,2 +1,11 @@
 -- Fetch all the albums which are released between 1990 and 2000 (including both years).
 -- Sort the result in descending order by the release year.
+select
+  name
+  , year
+from
+  album
+where
+  year between 1990 and 2000
+order by
+  year desc;
