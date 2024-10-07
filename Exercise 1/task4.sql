a8b9d8a5e03f66c6632f1ee75c8f534526051e0f
SanPut <santeriandre@gmail.com>
2024-10-07 18:17:11 +0300

    task4.sql: answer

--- Exercise 1/task4.sql
+++ Exercise 1/task4.sql
@@ -1,3 +1,13 @@
 -- Retrieve name and length for tracks which length is less than one and half minutes.
--- Sort the result primarily in ascending order by the name and secondarily 
+-- Sort the result primarily in ascending order by the name and secondarily
 -- in descending order by the length.
+select
+  name
+  , length
+from
+  track
+where
+  length < 130
+order by
+  name asc
+  , length desc;
