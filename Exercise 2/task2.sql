fc3f34982d5afe909216b3bb3bf21719c3ab8e3e
SanPut <santeriandre@gmail.com>
2024-10-08 13:56:45 +0300

    task2.sql: answer

--- Exercise 2/task2.sql
+++ Exercise 2/task2.sql
@@ -1,3 +1,20 @@
 -- Fetch album and track information for the artist "Iron Maiden".
 -- Select Album name, album year, track name, track number and track length to the result.
 -- Sort the result primarily by album name and secondarily by track number.
+select
+  album.name
+  , album.year
+  , track.name
+  , track.track_number
+  , track.length
+from
+  album
+  join artist
+on album.artist_id = artist.id
+join track
+on album.id = track.album_id
+where
+  artist.name = "Iron Maiden"
+order by
+  album.name
+  , track.track_number;
