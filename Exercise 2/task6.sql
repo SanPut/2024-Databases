be186c92f3f661ae746942162f1a7e44b2bda662
SanPut <santeriandre@gmail.com>
2024-10-08 13:57:17 +0300

    task6.sql: answer

--- Exercise 2/task6.sql
+++ Exercise 2/task6.sql
@@ -1,3 +1,12 @@
 -- Calculate and return the count of albums each band has in the database.
 -- Include Artist name and album count into the result.
 -- Name the result columns as "Artist" and "Album count".
+select
+  artist.name as "Artist"
+  , count(album.id) as "Album count"
+from
+  artist
+  join album
+on artist.i d = album.artist_id
+group by
+  artist.name;
