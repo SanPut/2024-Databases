19a14e701819398327641ec16f6f3121841be60f
SanPut <santeriandre@gmail.com>
2024-10-08 13:57:11 +0300

    task5.sql: answer

--- Exercise 2/task5.sql
+++ Exercise 2/task5.sql
@@ -1,3 +1,11 @@
 -- Fetch artist name and their albums. Include albums without artist information in the
 -- result set.
 -- Include artist's name, albums name and release year to the result.
+select
+  artist.name as artist_name
+  , album.name as album_name
+  , album.year
+from
+  artist
+  join album
+on artist.id = album.artist_id;
