4cef47a7278f097d13df58a5633ef96dac05c886
SanPut <santeriandre@gmail.com>
2024-11-22 13:41:30 +0200

    task2.sql: answer

--- Exercise 3/task2.sql
+++ Exercise 3/task2.sql
@@ -5,3 +5,18 @@
 -- the result. Order the result according the number of sales in descending order. 
 -- Don't include employees that have not sold anything in the result.
 
+select 
+    e.first_name as first_name,
+    e.last_name as last_name,
+    e.job_title as title,
+    COUNT(o.id) as number_of_sales
+from 
+    employees e
+join 
+    orders o ON e.id = o.employee_id
+group by 
+    e.id, e.first_name, e.last_name, e.job_title
+having 
+    COUNT(o.id) > 0
+order by 
+    number_of_sales DESC;
