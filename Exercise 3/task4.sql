90fe0ef3de7acb6d388bc38ff293d8949597e982
SanPut <santeriandre@gmail.com>
2024-11-22 13:43:00 +0200

    task4.sql: answer

--- Exercise 3/task4.sql
+++ Exercise 3/task4.sql
@@ -2,3 +2,15 @@
 -- each of the products. Write a query which returns all company's products 
 -- (the name of the product) and the number of orders. Include the products which are 
 -- not part of any order in the result.
+
+select 
+    p.product_name as product_name,
+    count(od.order_id) as number_of_orders
+from 
+    products p
+left join 
+    order_details od on p.id = od.product_id
+group by 
+    p.product_name
+order by 
+    number_of_orders desc;
