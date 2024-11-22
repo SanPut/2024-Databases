15f59811c046b11e82cbf4e2e78c2c870dae2f58
SanPut <santeriandre@gmail.com>
2024-11-22 13:41:15 +0200

    task1.sql: answer

--- Exercise 3/task1.sql
+++ Exercise 3/task1.sql
@@ -3,3 +3,14 @@
 -- that are ordered to New York. Include customer's first and last name, company and
 -- the order's shipping date into the result. Make sure orders which are not shipped yet
 -- are included in the result.
+select 
+    c.first_name as first_name,
+    c.last_name as last_name,
+    c.company as company_name,
+    o.shipped_date as shipped_date
+from 
+    orders o
+join 
+    customers c on o.customer_id = c.id
+where 
+    o.ship_city = 'New York' or o.shipped_date is null;
