af9bfaaf8fe795323524961b5dad31a5cde3d070
SanPut <santeriandre@gmail.com>
2025-01-01 17:08:48 +0200

    task5.sql: answer

--- Exercise 3/task5.sql
+++ Exercise 3/task5.sql
@@ -21,3 +21,31 @@ When an ingredient is deleted, the delete operation is blocked if there are any 
 which contains the ingredient. When a product is deleted, the product_id in the Contains 
 table is set to NULL.
 */
+
+create database Products;
+
+use Products;
+
+create table Product (
+    id int primary key,
+    name varchar(255)
+);
+
+create table Ingredient (
+    id int primary key,
+    name varchar(255),
+    calories decimal(10, 2)
+);
+
+create table Contains (
+    ingredient_id int,
+    product_id int,
+    amount decimal(10, 2),
+    primary key (ingredient_id, product_id),
+    foreign (ingredient_id) references Ingredient(id)
+        on update cascade
+        on delete restrict,
+    foreign key (product_id) references Product(id)
+        on update casade
+        on delete set null
+);
